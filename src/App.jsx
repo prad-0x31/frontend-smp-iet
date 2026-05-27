745584972f84c7bfbb0c29ba6bcdae2bb692e227
Pradyoth Nadig <249601562+prad-0x31@users.noreply.github.com>
2026-05-28 03:23:17 +0530

    Add CartSidebar to the page
    Using the existing CartSidebar component, place the CartSidebar in the
    botton right of the page by adding it to App.jsx with the main app and
    using flex to make the main app flex and the cart have a fixed width.
    
    Modified: App.jsx, index.css

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,24 @@ import ItemPage from "./pages/ItemPage";
 import About from "./pages/About";
 import Collections from "./pages/Collections";
 import Contact from "./pages/Contact";
+import CartSidebar from './components/CartSidebar'
 
 function App() {
   return (
     <div className="app">
       <Headbar />
-
-      <main className="app-content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/collections" element={<Collections />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/item/:id" element={<ItemPage />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </main>
+      <div className="app-layout">
+        <main className="app-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/collections" element={<Collections />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/item/:id" element={<ItemPage />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </main>
+      <div className="sideBar"><CartSidebar /></div>
+      </div>
     </div>
   );
 }
